4cc303ea6d6f32bce6fd7534c2ebae9d49a3ec71
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-10 18:34:16 +0300

    feat: added visibility modifier handling

--- src/main/groovy/com/github/eugenelesnov/OptionalGetterTransformation.groovy
+++ src/main/groovy/com/github/eugenelesnov/OptionalGetterTransformation.groovy
@@ -2,6 +2,7 @@ package com.github.eugenelesnov
 
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.ast.*
+import org.codehaus.groovy.ast.expr.ConstantExpression
 import org.codehaus.groovy.ast.expr.Expression
 import org.codehaus.groovy.ast.expr.StaticMethodCallExpression
 import org.codehaus.groovy.ast.stmt.ReturnStatement
@@ -10,6 +11,7 @@ import org.codehaus.groovy.control.SourceUnit
 import org.codehaus.groovy.transform.AbstractASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
 
+import static com.github.eugenelesnov.Visibility.parseOpCode
 import static org.codehaus.groovy.ast.ClassHelper.make
 
 @CompileStatic
@@ -27,16 +29,16 @@ class OptionalGetterTransformation extends AbstractASTTransformation {
             AnnotatedNode annotatedField = nodes[1] as AnnotatedNode
 
             if (annotatedField instanceof FieldNode) {
-                visitNode(annotatedField)
+                visitNode(annotation, annotatedField)
             }
         }
     }
 
-    private static void visitNode(FieldNode annotatedField) {
+    private static void visitNode(AnnotationNode annotation, FieldNode annotatedField) {
         if (isFieldValidForAST(annotatedField)) {
             annotatedField.getOwner().addMethod(
                     "get" + annotatedField.getName().capitalize() + "Optional",
-                    ACC_PUBLIC,
+                    getVisibilityProperty(annotation),
                     new ClassNode(Optional.class),
                     Parameter.EMPTY_ARRAY,
                     ClassNode.EMPTY_ARRAY,
@@ -49,6 +51,11 @@ class OptionalGetterTransformation extends AbstractASTTransformation {
         new ReturnStatement(
                 new StaticMethodCallExpression(new ClassNode(Optional.class), "ofNullable", valueToReturn)
         )
+    }
+
+    private static int getVisibilityProperty(AnnotationNode annotation) {
+        def visibility = annotation.getMember("visibility").getProperties()["property"] as ConstantExpression
+        return parseOpCode(visibility.getValue().toString())
     }
 
     private static boolean isFieldValidForAST(FieldNode annotatedField) {
--- src/test/groovy/com/github/eugenelesnov/GenerateOptionalGetterMethodTest.groovy
+++ src/test/groovy/com/github/eugenelesnov/GenerateOptionalGetterMethodTest.groovy
@@ -1,6 +1,7 @@
 package com.github.eugenelesnov
 
 import groovy.transform.ASTTest
+import groovyjarjarasm.asm.Opcodes
 import org.codehaus.groovy.ast.ClassNode
 import org.codehaus.groovy.ast.MethodNode
 
@@ -16,12 +17,15 @@ import static org.codehaus.groovy.control.CompilePhase.CANONICALIZATION
             .findAny()
             .orElseThrow()
 
-    assert methodNode.getName() == "getSomeFieldOptional"
-    println methodNode.getTypeDescriptor()
+    andThen: 'classNode contains getter method that returns Optional'
+    assert methodNode.getReturnType().getTypeClass() == Optional
+
+    andThen: 'generated method should have protected modifier'
+    assert methodNode.getModifiers() == Opcodes.ACC_PROTECTED
 })
 class GenerateOptionalGetterMethodTest {
 
-    @OptionalGetter
+    @OptionalGetter(visibility = Visibility.PROTECTED)
     private String someField = "testValue"
 
 }
