5825bfaa20768975acc52a3e467a4e06f48e43fc
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-11 00:02:50 +0300

    feat: changed retention policy for annotation

--- src/main/groovy/com/github/eugenelesnov/OptionalGetter.groovy
+++ src/main/groovy/com/github/eugenelesnov/OptionalGetter.groovy
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.FIELD)
 @GroovyASTTransformationClass(classes = [OptionalGetterTransformation])
 @interface OptionalGetter {
