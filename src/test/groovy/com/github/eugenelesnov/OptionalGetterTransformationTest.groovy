3810bf6177b3d93bc8dc84ff9699b70414c233c7
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-11 21:38:54 +0300

    test: added tests for different types of visibility modifier

--- src/test/groovy/com/github/eugenelesnov/OptionalGetterTransformationTest.groovy
+++ src/test/groovy/com/github/eugenelesnov/OptionalGetterTransformationTest.groovy
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class OptionalGetterTransformationTest extends GroovyAssert {
 
     @Test
-    void 'should generate optional getter'() {
+    void 'should generate optional getter with public modifier'() {
         assertScript '''
         import com.github.eugenelesnov.OptionalGetter
         import com.github.eugenelesnov.Visibility
@@ -14,10 +14,6 @@ class OptionalGetterTransformationTest extends GroovyAssert {
         class Foo {
             @OptionalGetter(visibility = Visibility.PUBLIC)
             private String someField
-
-            void setSomeField(String someField) {
-                this.someField = someField
-            }
         }
         
         void main() {
@@ -26,6 +22,55 @@ class OptionalGetterTransformationTest extends GroovyAssert {
 
             def getter = foo.getClass().getDeclaredMethod("getSomeFieldOptional", new Class[] {})
             assert getter != null
+            assert Arrays.asList(getter.getModifiers()).contains(Opcodes.ACC_PUBLIC)
+            assert getter.getReturnType() instanceof Optional
+        }
+'''
+    }
+
+    @Test
+    void 'should generate optional getter with protected modifier'() {
+        assertScript '''
+        import com.github.eugenelesnov.OptionalGetter
+        import com.github.eugenelesnov.Visibility
+import jdk.internal.org.objectweb.asm.Opcodes
+
+        class Foo {
+            @OptionalGetter(visibility = Visibility.PROTECTED)
+            private String someField
+        }
+        
+        void main() {
+            Foo foo = new Foo()
+            foo.setSomeField("value")
+
+            def getter = foo.getClass().getDeclaredMethod("getSomeFieldOptional", new Class[] {})
+            assert getter != null
+            assert Arrays.asList(getter.getModifiers()).contains(Opcodes.ACC_PROTECTED)
+            assert getter.getReturnType() instanceof Optional
+        }
+'''
+    }
+
+    @Test
+    void 'should generate optional getter with private modifier'() {
+        assertScript '''
+        import com.github.eugenelesnov.OptionalGetter
+        import com.github.eugenelesnov.Visibility
+import jdk.internal.org.objectweb.asm.Opcodes
+
+        class Foo {
+            @OptionalGetter(visibility = Visibility.PRIVATE)
+            private String someField
+        }
+        
+        void main() {
+            Foo foo = new Foo()
+            foo.setSomeField("value")
+
+            def getter = foo.getClass().getDeclaredMethod("getSomeFieldOptional", new Class[] {})
+            assert getter != null
+            assert Arrays.asList(getter.getModifiers()).contains(Opcodes.ACC_PRIVATE)
             assert getter.getReturnType() instanceof Optional
         }
 '''
